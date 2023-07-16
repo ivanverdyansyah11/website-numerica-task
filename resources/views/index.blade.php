3c8db02ff218b0d9059c2008a7e00b916094ffd4
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-16 12:27:51 +0800

    Progress: Finish Section Hero & Service

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,158 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
-<body>
+<body style="padding-bottom: 100px;">
+
+    {{-- NAVBAR --}}
+    <nav class="navbar navbar-expand-lg py-3 position-relative">
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand" href="#home">
+                <img src="{{ asset('assets/img/logo/logo-brand.svg') }}" class="img-fluid" alt="Logo Brand">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-lg-flex justify-content-lg-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link py-2 py-lg-0 px-lg-3 active" href="">Home</a>
+                    <a class="nav-link py-2 py-lg-0 px-lg-3" href="">How we help</a>
+                    <a class="nav-link py-2 py-lg-0 px-lg-3" href="">Pricing</a>
+                    <a class="nav-link py-2 py-lg-0 px-lg-3" href="">Our Offering</a>
+                </div>
+            </div>
+            <a href="#" class="button-default-small d-none d-lg-inline-block">Contact Us</a>
+        </div>
+    </nav>
+    {{-- END NAVBAR --}}
+
+
+    <main>
+        {{-- HERO SECTION --}}
+        <section class="hero d-flex align-items-center py-4 py-lg-0 pb-xl-5 pb-xxl-0 position-relative" id="hero">
+            <div class="banner-image-reverse position-absolute d-none d-lg-inline-block">
+                <img src="{{ asset('assets/img/banner/hero-banner.svg') }}" class="img-fluid w-100 h-100"
+                    alt="hero Banner Image">
+            </div>
+            <div class="container position-relative">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <h1 class="headline" style="margin-bottom: 20px;">Unlock the Power of Numbers with Numerica,
+                            Your Trusted Accounting Partner</h1>
+                        <div class="wrapper-paragraph d-flex flex-column gap-2" style="margin-bottom: 42px;">
+                            <p class="paragraph">Welcome to Numerica, where numbers come to life and financial clarity
+                                is our expertise. As a leading accounting firm, we are committed to providing accurate,
+                                reliable, and comprehensive financial solutions tailored to meet your specific needs.
+                            </p>
+                            <p class="paragraph d-none d-md-inline-block">Whether you're an individual, a small
+                                business, or a large corporation, Numerica is here to guide you towards financial
+                                success with our unparalleled expertise and personalized approach. Together we'll pave
+                                the path to a prosperous future.</p>
+                        </div>
+                        <div class="wrapper-button d-flex gap-2 align-items-center">
+                            <a href="#" class="button-default">Get Started</a>
+                            <a href="#" class="button-dark d-flex align-items-center gap-2">
+                                Request a Consultation
+                                <div class="arrow-icon"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{-- END HERO SECTION --}}
+
+
+        <div class="container">
+            {{-- SERVICE SECTION --}}
+            <section class="service section-gap" id="service">
+                <div class="container">
+                    <div class="row align-items-end justify-content-between row-gap">
+                        <div class="col-lg-6 mb-2 mb-lg-0">
+                            <h3 class="title">Empowering Your Financial Success, Discover the Numerica Difference</h3>
+                        </div>
+                        <div class="col-lg-5">
+                            <p class="paragraph">From bookkeeping to financial statement preparation, our comprehensive
+                                accounting services ensure accuracy, compliance, and clarity in your financial records.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="card-default">
+                                <div class="card-image" style="margin-bottom: 14px">
+                                    <img src="{{ asset('assets/img/service/service-1.svg') }}" class="img-fluid"
+                                        alt="Service Image">
+                                </div>
+                                <h6 style="margin-bottom: 4px;">Financial Reporting</h6>
+                                <p class="paragraph-small" style="margin-bottom: 18px">We provide accurate and
+                                    up-to-date bookkeeping services to
+                                    ensure your financial records are organized and compliant.</p>
+                                <a href="#" class="card-link d-flex gap-2">
+                                    More Detail
+                                    <img src="{{ asset('assets/img/icon/arrow-primary.svg') }}" class="img-fluid"
+                                        alt="Arrow Button Primary">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card-default">
+                                <div class="card-image" style="margin-bottom: 14px">
+                                    <img src="{{ asset('assets/img/service/service-2.svg') }}" class="img-fluid"
+                                        alt="Service Image">
+                                </div>
+                                <h6 style="margin-bottom: 4px;">Tax Planning and Preparation</h6>
+                                <p class="paragraph-small" style="margin-bottom: 18px">Our team of tax experts helps you
+                                    navigate the complex tax landscape, offering strategic tax planning to minimize
+                                    liabilities.</p>
+                                <a href="#" class="card-link d-flex gap-2">
+                                    More Detail
+                                    <img src="{{ asset('assets/img/icon/arrow-primary.svg') }}" class="img-fluid"
+                                        alt="Arrow Button Primary">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card-default">
+                                <div class="card-image" style="margin-bottom: 14px">
+                                    <img src="{{ asset('assets/img/service/service-3.svg') }}" class="img-fluid"
+                                        alt="Service Image">
+                                </div>
+                                <h6 style="margin-bottom: 4px;">Financial Analysis</h6>
+                                <p class="paragraph-small" style="margin-bottom: 18px">Our financial forecasting
+                                    services help you make informed decisions, identify trends, and plan for future
+                                    growth and profitability.</p>
+                                <a href="#" class="card-link d-flex gap-2">
+                                    More Detail
+                                    <img src="{{ asset('assets/img/icon/arrow-primary.svg') }}" class="img-fluid"
+                                        alt="Arrow Button Primary">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card-default">
+                                <div class="card-image" style="margin-bottom: 14px">
+                                    <img src="{{ asset('assets/img/service/service-4.svg') }}" class="img-fluid"
+                                        alt="Service Image">
+                                </div>
+                                <h6 style="margin-bottom: 4px;">Payroll Services</h6>
+                                <p class="paragraph-small" style="margin-bottom: 18px">We handle payroll calculations,
+                                    tax deductions, and reporting, helping you stay compliant with payroll regulations.
+                                </p>
+                                <a href="#" class="card-link d-flex gap-2">
+                                    More Detail
+                                    <img src="{{ asset('assets/img/icon/arrow-primary.svg') }}" class="img-fluid"
+                                        alt="Arrow Button Primary">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {{-- END SERVICE SECTION --}}
+        </div>
+    </main>
 
 
     {{-- SCRIPT JS --}}
